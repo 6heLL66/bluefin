9699121c9e0e734652a7d656ecbe6088934ac09b
Никита Семенчук <n.semenchuk@texture.finance>
2025-09-11 02:52:43 +0300

    asd

--- app/src/own-api/getLighterMarkets.ts
+++ app/src/own-api/getLighterMarkets.ts
@@ -1,7 +1,7 @@
-import { TokenDto_Output } from "../api"
+import { TokenDto_Output } from '../api'
 
 export const getLighterMarkets = async () => {
   const response = await fetch('https://mainnet.zklighter.elliot.ai/api/v1/orderBookDetails')
   const data = await response.json()
-  return data.order_book_details as TokenDto_Output[]
+  return data.order_book_details.map((item: TokenDto_Output & { last_trade_price: string }) => ({ ...item, price: item.last_trade_price })) as TokenDto_Output[]
 }
